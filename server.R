f6d1be2e929606786ba3841b8349b3987cea8359
Tim Blokker <timflorianblokker@gmail.com>
2020-10-12 20:03:11 +0200

    formatted into functions and link back to transitions with colnames

--- server.R
+++ server.R
@@ -6,99 +6,120 @@ shinyServer(function(input, output) {
 
 observeEvent(input$start, {
 
-  logTransformation = TRUE
-  tree_file = input$tree_file_w_transitions$datapath
-  distances_file = input$distances_file$datapath
-  source("readT.R")
-  
   output$plot = renderPlot({
     
-    tree_file = "input/h3_small_sample.MCC.tre"
-    #distances_file = "bedford/subsetDeff.txt"
-    distances_file = "input/subsetDeff.txt"
-    
-    countries<-unique(read.table("input/Sampling_locations.txt")[,2])
-    
-    tree = readT(tree_file)
-    distances = read.csv(distances_file, head=T, sep="\t")
-    head(print(tree)); print(distances)
-    
-    locations = colnames(distances)
-    transitions = matrix(0, nrow=dim(distances)[1], ncol=dim(distances)[1])#0 matrix in size of distance matrix 
-    row.names(transitions) = countries 
-    colnames(transitions) = countries
-    for (i in 1:dim(tree$edge)[1])#the edge table should be read as: rows are the edge numbers and first col is start node  and 2nd is end node of the branch or edge
-    {
-      if (tree$edge[i,1]%in%tree$edge[,2])#if a start node is also a end node, then we are going from somewhere to that city. We want to assign the start city as location 1
-      {
-        index = which(tree$edge[,2]==tree$edge[i,1])#get branch that has node of interest as end node
-        location1 = tree$annotations[[index]]$city #assign start node of branch as location1
-        if(is.list(location1))
-          {
-          location1 = tree$annotations[[index]]$city[[1]]#take arbritarily the first MP ancestral state
-        }
-      }	else	{
-        location1 = tree$root.annotation$city#tip nodes are not in column 1 so if it is not an internal node it is then the root node
-        if(is.list(location1))
-          {
-          location1 = tree$root.annotation$city[[1]]
-        }
-      }
-      location2 = tree$annotations[[i]]$city#location 2 is the start of the branch we are at atm
-      if(is.list(location2))
-      {
-        location2 = tree$annotations[[i]]$city[[1]]
-      }
-      transitions[location1,location2] = transitions[location1,location2]+1
-    }
 
-    full_transitions=transitions
-    transitions[which(transitions!=0)]
-    distances = distances[lower.tri(distances)]
-    transitions = transitions[lower.tri(transitions)]#assign true to lower triangle values
-    distances = distances[which(transitions!=0)]
-    transitions = transitions[which(transitions!=0)]
-    if (logTransformation == TRUE) distances = log(distances)
-    
-    par(mgp=c(0,0,0), oma=c(0,0,0,0), mar=c(3,3.5,1.5,2))
-    cols1 = c("#FAA521","#4676BB"); cols2 = c("#FAA52150","#4676BB50")
-    plot(distances, transitions, col=cols2[2], pch=16, cex=1.1, axes=F, ann=F)
-    points(distances, transitions, col=cols1[2], pch=1, cex=1.1)
-    axis(side=1, lwd.tick=0.2, cex.axis=0.9, mgp=c(0,0.4,0), lwd=0.2, tck=-0.02, col.tick="gray30", col.axis="gray30", col="gray30")
-    axis(side=2, lwd.tick=0.2, cex.axis=0.9, mgp=c(0,0.6,0), lwd=0.2, tck=-0.02, col.tick="gray30", col.axis="gray30", col="gray30")
-    title(ylab="transitions", cex.lab=1.1, mgp=c(2.0,0,0), col.lab="gray30")
-    if (logTransformation == TRUE) title(xlab="distance (log)", cex.lab=1.1, mgp=c(1.4,0,0), col.lab="gray30")
-    if (logTransformation != TRUE) title(xlab="distance", cex.lab=1.1, mgp=c(1.4,0,0), col.lab="gray30")
-    
-    
-    #LINEAR REGRESSION
+
+transitions_raw_fun = function(distances_raw_file="input/subsetDeff.txt", city = "states") {
+  distances_raw <- read.csv(distances_raw_file, head=T, sep="\t")
+  
+  locations = colnames(distances_raw)
+  transitions_raw= matrix(0, nrow=dim(distances_raw)[1], ncol=dim(distances_raw)[1])#0 matrix in size of distance matrix 
+  row.names(transitions_raw) = countries 
+  colnames(transitions_raw) = countries
+  
+  for (i in 1:dim(tree$edge)[1])
+    #the edge table should be read as: rows are the edge numbers and first col is start node  and 2nd is end node of the branch or edge
+  {
+    if (tree$edge[i, 1] %in% tree$edge[, 2])
+      #if a start node is also an end node, then we are going from somewhere to that city. We want to assign the start city as location 1
+    {
+      index = which(tree$edge[, 2] == tree$edge[i, 1])#get node that is end node of edge in question
+      location1 = get(city, tree$annotations[[index]]) #assign end node of branch as location1
+      if (is.list(location1))
+      {
+        location1 = get(city, tree$annotations[[index]])[[1]]#take arbritarily the first MP ancestral state
+      }
+    }	else	{
+      location1 = get(city, tree$root.annotation)#tip nodes are not in column 1 so if it is not an internal node it is then the root node
+      if (is.list(location1))
+      {
+        location1 = get(city, tree$root.annotation)[[1]]
+      }
+    }
+    location2 = get(city, tree$annotations[[i]])#location 2 is the start of the branch we are at atm
+    if (is.list(location2))
+    {
+      location2 = get(city, tree$annotations[[i]])[[1]]
+    }
+    if (nchar(location2) > 2 | nchar(location1) > 2) {
+      location2 = substr(location2, start = 1, stop = 2)
+      location1 = substr(location1, start = 1, stop = 2)
+    }
+    transitions_raw[location1, location2] = transitions_raw[location1, location2] + 1
+  }
+  list=list(transitions_raw=transitions_raw,distances_raw=distances_raw)
+  return(list)
+}
+
+plotting_prep <- function(makeSymmetric = TRUE, transitions_raw, distances_raw) {
+  transitions=transitions_raw
+  transitions_added=transitions_raw
+  if (makeSymmetric == TRUE){
+    for (i in colnames(transitions)){
+      for (j in colnames(transitions)){
+        transitions_added[i, j] <- transitions[j, i] + transitions[i, j]
+      }
+    }
+    transitions <- transitions_added
+  }
+  transitions = transitions[lower.tri(transitions)]#assign true to lower triangle values
+  transitions = transitions[which(transitions != 0)]
+  distances = distances_raw[lower.tri(distances_raw)]
+  distances = distances[which(transitions != 0)]
+  list<-list(transitions=transitions, distances=distances, transitions_added= transitions_added)
+  return(list)
+}
+
+plotting_fun<-function(logTransformation=TRUE, distances, transitions){
+  if (logTransformation == TRUE) distances = log(distances)
+  par(mgp=c(0,0,0), oma=c(0,0,0,0), mar=c(3,3.5,1.5,2))
+  cols1 = c("#FAA521","#4676BB"); 
+  cols2 = c("#FAA52150","#4676BB50")
+  plot(distances, transitions, col=cols2[2], pch=16, cex=1.1, axes = F, ann=F)
+  points(distances, transitions, col=cols1[2], pch=1, cex=1.1)
+  axis(side=1, lwd.tick=0.2, cex.axis=0.9, mgp=c(0,0.4,0), lwd=0.2, tck=-0.02, col.tick="gray30", col.axis="gray30", col="gray30")
+  axis(side=2, lwd.tick=0.2, cex.axis=0.9, mgp=c(0,0.6,0), lwd=0.2, tck=-0.02, col.tick="gray30", col.axis="gray30", col="gray30")
+  title(ylab="transitions", cex.lab=1.1, mgp=c(2.0,0,0), col.lab="gray30")
+  if (logTransformation == TRUE) title(xlab="distance (log)", cex.lab=1.1, mgp=c(1.4,0,0), col.lab="gray30")
+  if (logTransformation != TRUE) title(xlab="distance", cex.lab=1.1, mgp=c(1.4,0,0), col.lab="gray30")
+}
+
+linear_regression<-function(x=30){
     abline(lm(transitions ~ distances))
     lm=lm(transitions~distances)
     plot(lm$residuals)
-    which(lm$residuals>10) 
-    
-    #86 can I now trace this back to which transition this is?
-    
-    transitions[86]
-    possible_outliers=which(y==13)
-    
-    rows=rep(0,4)
-    columns=rep(0,4)
-    rc=matrix(as.character(c(rows,columns)),4)
-    for (i in 1:length(possible_outliers))
-    {
-      rows[i]=ceiling(possible_outliers[i]/dim(y)[1])
-      columns[i]=possible_outliers[i]%%dim(y)[1]
-      rc[i,]=c(colnames(y)[rows[i]],colnames(y)[columns[i]])
-    }
-    rc
-    title("ML - Geographical distance")
+    #TODO make this also applicable when there are more than one posible outlier.
+    df_transitions_added<-data.frame(transitions_added)
+    transitions[which(lm$residuals>x)[[1]]]
+    possible_outliers=which(df_transitions_added==69)
+    print("Possible Outlier:" )
+    print(colnames(df_transitions_added[ceiling(possible_outliers/dim(df_transitions_added)[1])]))
+    colnames(df_transitions_added[possible_outliers%%dim(df_transitions_added)[1]])
     summary(lm)
+}   
 
-    
-      #extend functionality with the possibility to upload a tree that does not yet contain the information of transitions between locations
-#create function that computes the transition frequencies using maximum likelihood or parsimony and then output enhanced tree file
-    source("AncestralReconstruction.R")#maybe better to keep everyhtin in server
+
+source("readT.R")
+tree_file="input/h3_small_sample.MCC.tre"
+tree <-readT(tree_file)
+
+sampling.locations="input/Sampling_locations.txt"
+countries<-levels(read.table(sampling.locations)[,2])
+#source("AncestralReconstruction.R")
+
+distances_raw=transitions_raw_fun(distances_raw_file="input/subsetDeff.txt", city = "states")$distances_raw
+transitions_raw=transitions_raw_fun(distances_raw_file="input/subsetDeff.txt", city = "states")$transitions_raw
+
+transitions<-plotting_prep(makeSymmetric=TRUE, distances_raw = distances_raw, transitions_raw = transitions_raw)$transitions
+distances<-plotting_prep(makeSymmetric=TRUE, distances_raw = distances_raw, transitions_raw = transitions_raw)$distances
+transitions_added<-plotting_prep(makeSymmetric=TRUE, distances_raw = distances_raw, transitions_raw = transitions_raw)$transitions_added
+
+plotting_fun(logTransformation = FALSE, distances = distances, transitions=transitions)
+
+linear_regression(x=40)
+
+
 
 }) # output$plot = renderPlot({
 }) # observeEvent(input$start, {
